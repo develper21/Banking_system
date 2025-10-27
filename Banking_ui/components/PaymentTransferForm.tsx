18f8e6d81266217c2e8af64f166d129a1ca3c66e
develper21 <githubnarvin@gmail.com>
2025-10-27 23:14:47 +0530

    remove comments and set batter structure

--- Banking_ui/components/PaymentTransferForm.tsx
+++ Banking_ui/components/PaymentTransferForm.tsx
@@ -6,12 +6,10 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-
 import { createTransfer } from "@/lib/actions/dwolla.actions";
 import { createTransaction } from "@/lib/actions/transaction.actions";
 import { getBank, getBankByAccountId } from "@/lib/actions/user.actions";
 import { decryptId } from "@/lib/utils";
-
 import { BankDropdown } from "./BankDropdown";
 import { Button } from "./ui/button";
 import {
