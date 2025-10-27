5c6c282f7cacd7df1c1a32661d953f61116369d3
develper21 <githubnarvin@gmail.com>
2025-10-27 23:15:14 +0530

    remove comments and set batter structure

--- Banking_ui/components/ui/progress.tsx
+++ Banking_ui/components/ui/progress.tsx
@@ -2,12 +2,16 @@
 
 import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
-
 import { cn } from "@/lib/utils";
+
+interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorClassName?: string;
+}
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  ProgressProps
 >(({ indicatorClassName, className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
@@ -22,10 +26,11 @@ const Progress = React.forwardRef<
         "h-full w-full flex-1 bg-primary transition-all",
         indicatorClassName
       )}
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ));
-Progress.displayName = ProgressPrimitive.Root.displayName;
+
+Progress.displayName = "Progress";
 
 export { Progress };
