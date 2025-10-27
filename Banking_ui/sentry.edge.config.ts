ae04d419909f25748bef0c09a1914f1c8da82fa9
develper21 <githubnarvin@gmail.com>
2025-10-27 23:11:39 +0530

    create for sentry edge config file

--- Banking_ui/sentry.edge.config.ts
+++ Banking_ui/sentry.edge.config.ts
@@ -6,11 +6,15 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://9f478aaa5c3d24194629a74e7849d1c1@o4507221127462912.ingest.us.sentry.io/4507221129297920",
+  dsn: "https://9370c3308401cc56804ccf10d28e2521@o4510262339960832.ingest.de.sentry.io/4510262588604496",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
