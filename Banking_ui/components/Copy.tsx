cc7a21e715045f9b5cc20209de39672e54a09969
develper21 <githubnarvin@gmail.com>
2025-10-27 23:14:23 +0530

    remove comments and set batter structure

--- Banking_ui/components/Copy.tsx
+++ Banking_ui/components/Copy.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { useState } from "react";
-
 import { Button } from "./ui/button";
 
 const Copy = ({ title }: { title: string }) => {
   const [hasCopied, setHasCopied] = useState(false);
-
   const copyToClipboard = () => {
     navigator.clipboard.writeText(title);
     setHasCopied(true);
